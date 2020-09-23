d3d5bb4d58f6f31ad8529cd2bd845604d0471965
josehailton <71135811+josehailton@users.noreply.github.com>
2020-09-23 10:17:43 -0300

    Update TestExercicio.java
    Atualizei os resultados esperados e defini os meus valores para modificar as variáveis.

--- src/test/java/com/example/project/TestExercicio.java
+++ src/test/java/com/example/project/TestExercicio.java
@@ -48,21 +48,27 @@ public class TestExercicio {
 	@Test
 	public void exercicio02_Customizado() {
 		//Defina seus próprios valores para as variáveis a seguir 
-		CupomFiscal.NOME_LOJA = "";
-		CupomFiscal.LOGRADOURO = "";
-		CupomFiscal.NUMERO = 0;
-		CupomFiscal.COMPLEMENTO = "";
-		CupomFiscal.BAIRRO = "";
-		CupomFiscal.MUNICIPIO = "";
-		CupomFiscal.ESTADO = "";
-		CupomFiscal.CEP = "";
-		CupomFiscal.TELEFONE = "";
-		CupomFiscal.OBSERVACAO = "";
-		CupomFiscal.CNPJ = "";
-		CupomFiscal.INSCRICAO_ESTADUAL = "";
+		CupomFiscal.NOME_LOJA = "Pibit.inc";
+		CupomFiscal.LOGRADOURO = "Rua José Pedro";
+		CupomFiscal.NUMERO = 110;
+		CupomFiscal.COMPLEMENTO = "Casa";
+		CupomFiscal.BAIRRO = "Virgilio Ribeiro";
+		CupomFiscal.MUNICIPIO = "Jacaraú";
+		CupomFiscal.ESTADO = "PB";
+		CupomFiscal.CEP = "58278-000";
+		CupomFiscal.TELEFONE = "(83) 9 8603-6511";
+		CupomFiscal.OBSERVACAO = "Próximo a Pizzaria";
+		CupomFiscal.CNPJ = "99.779.325/0001-81";
+		CupomFiscal.INSCRICAO_ESTADUAL = "324.632.739.801";
 		
 		//E atualize o texto esperado abaixo
-		rodarTestarOutput("" + BREAK);
+		rodarTestarOutput("Pibit.inc" + BREAK +
+				 		"Rua José Pedro, 110 Casa" + BREAK +
+						"Virgilio Ribeiro - Jacaraú - PB" + BREAK +
+						"CEP:58278-000 Tel (83) 9 8603-6511" + BREAK +
+						"Próximo a Pizzaria" + BREAK +
+						"CNPJ: 99.779.325/0001-81" + BREAK +
+						"IE: 324.632.739.801" + BREAK);
 	}
 
 	private void rodarTestarOutput(String expected) {
