f904a57127b0720b777d431e3b5108929a6ddfa8
josehailton <71135811+josehailton@users.noreply.github.com>
2020-09-23 11:46:02 -0300

    Update CupomFiscal.java
    Será que vai...

--- src/main/java/com/example/project/CupomFiscal.java
+++ src/main/java/com/example/project/CupomFiscal.java
@@ -14,35 +14,36 @@ public class CupomFiscal {
 	public static String OBSERVACAO = "Loja 1317 (PDP)";
 	public static String CNPJ = "42.591.651/0797-34";
 	public static String INSCRICAO_ESTADUAL = "244.898.500.113";
-	
+
+	private static String p_L = System.lineSeparator();
+
 	public static void exercicio01() {
-        	String imprimirCupom = NOME_LOJA + "\n";
-        	imprimirCupom += LOGRADOURO + ", " + NUMERO + " " + COMPLEMENTO + "\n";
-        	imprimirCupom += BAIRRO + " - " +MUNICIPIO+ " - " +ESTADO + "\n";
-        	imprimirCupom += "CEP: " + CEP + "Tel " + TELEFONE + "\n";
-        	imprimirCupom += OBSERVACAO + "\n";
-        	imprimirCupom += "CNPJ: " + CNPJ + "\n";
-        	imprimirCupom += "IE: " + INSCRICAO_ESTADUAL;
-        
-        	System.out.println(imprimirCupom);
-    	}
-	
-	private static String pular_linha = System.lineSeparator();
-    
-    	public void exercicio02_tudoVazio() {
-        	System.out.println(
-                	pular_linha +
-                	", " + pular_linha +
-                	" -  - " + pular_linha +
-                	"CEP: Tel " + pular_linha +
-                	pular_linha +
-                	"CNPJ: " + pular_linha +
-                	"IE: "
-        	);
-    	}
-	
+		String imprimirCupom = NOME_LOJA + p_L;
+		imprimirCupom += LOGRADOURO + ", " + NUMERO + " " + COMPLEMENTO + p_L; 
+		imprimirCupom += BAIRRO +" - " + MUNICIPIO + " - " + ESTADO + p_L;
+		imprimirCupom += "CEP:" + CEP + " Tel " + TELEFONE + p_L;
+		imprimirCupom += OBSERVACAO + p_L;
+		imprimirCupom += "CNPJ: " + CNPJ + p_L;
+		imprimirCupom += "IE: " + INSCRICAO_ESTADUAL;
+
+		System.out.println(imprimirCupom);
+
+	}
+
+	public void exercicio02_tudoVazio() {
+		System.out.println(
+			p_L +
+			", " + p_L +
+			" -  - " + p_L +
+			"CEP: Tel " + p_L +
+			p_L +
+			"CNPJ: " + p_L +
+			"IE: "
+		);
+	}
+
 	public static void main(final String[] args) {
 		exercicio01();
 	}
-	
+
 }
